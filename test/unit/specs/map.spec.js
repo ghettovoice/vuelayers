0354772a8a107e5c2a14fa61c6049fdab207b031
ghettovoice <ghettovoice@gmail.com>
2017-03-07 00:55:09 +0300

    Bump package version

--- test/unit/specs/map.spec.js
+++ test/unit/specs/map.spec.js
@@ -2,10 +2,13 @@ import Vue from 'vue'
 import Map from 'vl-components/map'
 
 describe('map/map.vue', () => {
-  it('should render correct contents', () => {
+  it('should render correct contents', done => {
     const Constructor = Vue.extend(Map)
     const vm = new Constructor().$mount()
     expect(vm.$el).to.have.class('vl-map')
-    expect(vm.$el).to.contain('canvas')
+    Vue.nextTick(() => {
+      expect(vm.$el).to.contain('canvas')
+      done()
+    })
   })
 })
