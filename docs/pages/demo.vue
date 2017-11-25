caad00ea227f82e6856f136d1a70cea599026436
ghettovoice <ghettovoice@gmail.com>
2017-11-25 19:37:36 +0300

    move demo to separate repository #22

--- docs/pages/demo.vue
+++ docs/pages/demo.vue
@@ -9,12 +9,22 @@
     section.section.content
       h3 Demo sources
 
+      b-message(type="is-success").
+        Now you can try the demo app yourself! Just clone the
+        #[a(href="https://github.com/ghettovoice/vuelayers-demo" target="_blank") vuelayers-demo] repository and deploy.
+      vld-code(lang="bash").
+        git clone https://github.com/ghettovoice/vuelayers-demo.git
+        cd vuelayers-demo
+        npm install
+        npm start
+      p.
+        After #[b Webpack] completes his work, open your browser and point it to #[code http://localhost:8080].#[br]
+        You should see the demo application like above.
+
       h4 main.js
       b-tabs
         b-tab-item(label="JS")
           vld-code(lang="js") {{ main.script }}
-        b-tab-item(label="SASS")
-          vld-code(lang="styl") {{ main.style }}
 
       h4 demo-app.vue
       b-tabs
@@ -28,11 +38,10 @@
 
 <script>
   import { extractScript, extractTemplate, extractStyle } from '../utils'
-  import DemoApp from '../../demo/app.vue'
+  import DemoApp from '../../modules/vuelayers-demo/src/App.vue'
   /* eslint-disable import/no-webpack-loader-syntax */
-  import demoSrc from '!raw-loader!../../demo/app.vue'
-  import mainScriptSrc from '!raw-loader!../../demo/main'
-  import mainStyleSrc from '!raw-loader!../../demo/main.sass'
+  import demoSrc from '!raw-loader!../../modules/vuelayers-demo/src/App.vue'
+  import mainScriptSrc from '!raw-loader!../../modules/vuelayers-demo/src/main'
   /* eslint-enable import/no-webpack-loader-syntax */
 
   const props = {
@@ -49,7 +58,6 @@
     main () {
       return {
         script: mainScriptSrc,
-        style: mainStyleSrc,
       }
     },
   }
