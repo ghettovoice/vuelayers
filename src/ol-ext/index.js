5305cc64f9d1f7b9524efca3d998e2db5de7c27d
ghettovoice <ghettovoice@gmail.com>
2018-02-21 09:38:35 +0300

    index files in mixin, ol-ext, rx-ext

---
+++ src/ol-ext/index.js
@@ -0,0 +1,13 @@
+/**
+ * @module ol-ext
+ */
+export * from './consts'
+export * from './extent'
+export * from './feature'
+export * from './format'
+export * from './geojson'
+export * from './geom'
+export * from './load-strategy'
+export * from './proj'
+export * from './style'
+export * from './tile-grid'
