4b210da9234a3e0e80f5950f1f105fb79c7d33a5
ghettovoice <ghettovoice@gmail.com>
2018-03-21 00:26:43 +0300

    serialization of circle geom

--- src/ol-ext/format.js
+++ src/ol-ext/format.js
@@ -1,5 +1,8 @@
-import GeoJSON from 'ol/format/geojson'
+import BaseGeoJSON from 'ol/format/geojson'
 import TopoJSON from 'ol/format/topojson'
+import { isEmpty } from '../util/minilo'
+import { createCircularPolygon } from './geom'
+import { isCircle } from './util'
 
 /**
  * @param {olx.format.GeoJSONOptions} [options]
@@ -15,4 +18,46 @@ export function createGeoJsonFmt (options) {
  */
 export function createTopoJsonFmt (options) {
   return new TopoJSON(options)
+}
+
+class GeoJSON extends BaseGeoJSON {
+  adaptOptions (options) {
+    return {
+      dataProjection: this.defaultDataProjection,
+      featureProjection: this.defaultFeatureProjection,
+      ...options,
+    }
+  }
+
+  writeGeometryObject (geometry, options) {
+    if (isCircle(geometry)) {
+      geometry = createCircularPolygon(geometry.getCenter(), geometry.getRadius())
+    }
+    return super.writeGeometryObject(geometry, options)
+  }
+
+  writeFeatureObject (feature, options) {
+    options = this.adaptOptions(options)
+    const object = /** @type {GeoJSONFeature} */ ({
+      'type': 'Feature',
+    })
+    const id = feature.getId()
+    if (id !== undefined) {
+      object.id = id
+    }
+    const geometry = feature.getGeometry()
+    if (geometry) {
+      object.geometry = this.writeGeometryObject(geometry, options)
+    } else {
+      object.geometry = null
+    }
+    const properties = feature.getProperties()
+    delete properties[feature.getGeometryName()]
+    if (!isEmpty(properties)) {
+      object.properties = properties
+    } else {
+      object.properties = null
+    }
+    return object
+  }
 }
