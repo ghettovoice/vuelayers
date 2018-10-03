5eaf32d17ce113f27fdfad1bd0a118f3b8b97087
ghettovoice <ghettovoice@gmail.com>
2018-10-03 21:53:21 +0300

    drop not used code

--- build/webpack/vue-jsdoc-loader/index.js
+++
@@ -1,9 +0,0 @@
-const path = require('path')
-
-module.exports = function () {
-  this.cacheable && this.cacheable()
-  const loaderPath = path.resolve(__dirname, './loader.js')
-  let query = this.query ? this.query + '&raw' : '?raw'
-
-  return `module.exports = require('!${loaderPath}${query}!jsdoc-loader?raw!${this.resource}')`
-}
