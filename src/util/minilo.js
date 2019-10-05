5ed402ad80c37d3c2ac6ad8d40ff336ff8dec8a8
Vladimir Vershinin <ghettovoice@gmail.com>
2019-10-05 13:41:38 +0300

    Add `kebabCase` helper

--- src/util/minilo.js
+++ src/util/minilo.js
@@ -315,6 +315,13 @@ export function camelCase (str) {
   return str.replace(regExp, matches => matches[1].toUpperCase())
 }
 
+export function kebabCase (str) {
+  return str.match(/[A-Z]{2,}(?=[A-Z][a-z0-9]*|\b)|[A-Z]?[a-z0-9]*|[A-Z]|[0-9]+/g)
+    .filter(Boolean)
+    .map(x => x.toLowerCase())
+    .join('-')
+}
+
 export function arrayLengthValidator (len) {
   return len => value => isArray(value) && value.length === len
 }
