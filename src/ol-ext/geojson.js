3eb1955d8c3e0ef0ae2f4e9689b75583722f9278
Vladimir Vershinin <ghettovoice@gmail.com>
2020-02-20 19:28:41 +0300

    Setup default projections in the geojson helpers

--- src/ol-ext/geojson.js
+++ src/ol-ext/geojson.js
@@ -1,16 +1,17 @@
 import { Feature } from 'ol'
 import { get, isPlainObject } from '../util/minilo'
 import { createGeoJsonFmt } from './format'
+import { EPSG_3857, EPSG_4326 } from './proj'
 
 const geoJsonFmt = createGeoJsonFmt()
 
 /**
  * @param {Feature} feature
- * @param {ProjectionLike|undefined} [featureProjection]
- * @param {ProjectionLike|undefined} [dataProjection]
+ * @param {ProjectionLike|undefined} [featureProjection=EPSG:3857]
+ * @param {ProjectionLike|undefined} [dataProjection=EPSG:4326]
  * @return {Object}
  */
-export function writeGeoJsonFeature (feature, featureProjection, dataProjection) {
+export function writeGeoJsonFeature (feature, featureProjection = EPSG_3857, dataProjection = EPSG_4326) {
   const geoJsonFeature = geoJsonFmt.writeFeatureObject(feature, { featureProjection, dataProjection })
 
   if (Array.isArray(get(geoJsonFeature, 'properties.features'))) {
@@ -27,11 +28,11 @@ export function writeGeoJsonFeature (feature, featureProjection, dataProjection)
 
 /**
  * @param {Object} geoJsonFeature
- * @param {ProjectionLike|undefined} [featureProjection]
- * @param {ProjectionLike|undefined} [dataProjection]
+ * @param {ProjectionLike|undefined} [featureProjection=EPSG:3857]
+ * @param {ProjectionLike|undefined} [dataProjection=EPSG:4326]
  * @return {Feature}
  */
-export function readGeoJsonFeature (geoJsonFeature, featureProjection, dataProjection) {
+export function readGeoJsonFeature (geoJsonFeature, featureProjection = EPSG_3857, dataProjection = EPSG_4326) {
   const feature = geoJsonFmt.readFeature(geoJsonFeature, { featureProjection, dataProjection })
 
   if (Array.isArray(feature.get('features'))) {
@@ -48,11 +49,11 @@ export function readGeoJsonFeature (geoJsonFeature, featureProjection, dataProje
 
 /**
  * @param {Geometry} geometry
- * @param {ProjectionLike|undefined} [geometryProjection]
- * @param {ProjectionLike|undefined} [dataProjection]
+ * @param {ProjectionLike|undefined} [geometryProjection=EPSG:3857]
+ * @param {ProjectionLike|undefined} [dataProjection=EPSG:4326]
  * @return {Object}
  */
-export function writeGeoJsonGeometry (geometry, geometryProjection, dataProjection) {
+export function writeGeoJsonGeometry (geometry, geometryProjection = EPSG_3857, dataProjection = EPSG_4326) {
   return geoJsonFmt.writeGeometryObject(geometry, {
     featureProjection: geometryProjection,
     dataProjection,
@@ -61,11 +62,11 @@ export function writeGeoJsonGeometry (geometry, geometryProjection, dataProjecti
 
 /**
  * @param {Object|Object} geoJsonGeometry
- * @param {ProjectionLike|undefined} [geometryProjection]
- * @param {ProjectionLike|undefined} [dataProjection]
+ * @param {ProjectionLike|undefined} [geometryProjection=EPSG:3857]
+ * @param {ProjectionLike|undefined} [dataProjection=EPSG:4326]
  * @return {Geometry}
  */
-export function readGeoJsonGeometry (geoJsonGeometry, geometryProjection, dataProjection) {
+export function readGeoJsonGeometry (geoJsonGeometry, geometryProjection = EPSG_3857, dataProjection = EPSG_4326) {
   dataProjection = readProjection(geoJsonGeometry, dataProjection)
 
   return geoJsonFmt.readGeometry(geoJsonGeometry, {
